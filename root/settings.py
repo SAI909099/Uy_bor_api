d13ac60e8fc965eaa58dacef95d35f5217afaf2c
SAI909099 <sulaymonovabdulaziz1@gmail.com>
2024-10-08 00:50:58 +0500

    env

--- root/settings.py
+++ root/settings.py
@@ -9,12 +9,16 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+import os
 from os.path import join
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-ntw(n3v&1z*m-9$@wk!48lqwct^jkpq2e5l6t$#t#7@ld-^2cv5'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 AUTH_USER_MODEL = 'apps.User'
